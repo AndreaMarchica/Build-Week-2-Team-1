752a58ee6dc3b67e26400a1693f7189b28baedd2
ZeroHenriot <darii.catalin@gmail.com>
2023-10-18 01:05:10 +0200

    search updates

--- serach.js
+++ serach.js
@@ -3,7 +3,8 @@ const genres = document.getElementById('genreRow')
 const row = document.querySelector('#content-row')
 const artistRow = document.querySelector('#artist-row')
 const artists = document.querySelector('#artists')
-const mostRelevant = document.querySelector('#songcontainer')
+let artistCardCount = 0
+let songsCardCount = 0
 // search.addEventListener('input', (e) => {
 //   const searched = e.target.value
 //   console.log(searched)
@@ -17,6 +18,8 @@ const query = 'search?q='
 // console.log('il tuo url', url)
 // console.log(query)
 
+search.value = ''
+
 search.addEventListener('input', () => {
   const searchTerm = search.value // Prendi il valore dell'input
   console.log('Ricerca:', searchTerm)
@@ -24,13 +27,11 @@ search.addEventListener('input', () => {
   if (searchTerm !== '') {
     genres.classList.add('d-none')
     row.classList.remove('d-none')
-    mostRelevant.classList.remove('d-none')
     artistRow.classList.remove('d-none')
     artists.classList.remove('d-none')
   } else {
     genres.classList.remove('d-none')
     row.classList.add('d-none')
-    mostRelevant.classList.add('d-none')
     artistRow.classList.add('d-none')
     artists.classList.add('d-none')
   }
@@ -54,50 +55,46 @@ search.addEventListener('input', () => {
         const data = events.data
         console.log('Ecco il risultato', data)
         row.innerHTML = ''
-        mostRelevant.innerHTML = ''
         artistRow.innerHTML = ''
+        artistCardCount = 0
+        songsCardCount = 0
         data.forEach((songs) => {
           const newCol = document.createElement('div')
-          newCol.classList.add('col', 'd-flex')
-          newCol.innerHTML = `
+          newCol.classList.add('col')
+          if (songsCardCount < 18) {
+            newCol.innerHTML = `
             <div class="card mb-4 shadow-sm d-flex">
-              <img
+            <button class="d-flex flex-column songsbutton">
+            <img
                 src="${songs.album.cover_medium}"
-                class="bd-placeholder-img card-img-top"
+                class="bd-placeholder-img card-img-top rounded w-50 m-2"
               />
-              <div class="card-body">
-                <p class="card-text">
+              <div class="card-body d-flex flex-column justify-content-between align-items-baseline text-nowrap text-truncate">
+                <p class="card-text fw-bold text-nowrap text-truncate m-0">
                   ${songs.title}
+                </p>
+                <p class="card-text text-nowrap text-truncate mb-0 mt-3">
+                  ${songs.artist.name}
                 </p>
                 <div
                   class="d-flex justify-content-between align-items-center"
                 >
-                  <div class="btn-group">
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      class="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                  </div>
+                 
                 </div>
               </div>
+              </button>
             </div>
           `
-          row.appendChild(newCol)
-          console.log(songs.type)
+            row.appendChild(newCol)
+            console.log(songs.type)
+          }
+          songsCardCount++
         })
 
         const uniqueData = {}
 
         data.forEach((songs) => {
-          if (!uniqueData[songs.artist.id]) {
+          if (!uniqueData[songs.artist.id] && artistCardCount < 7) {
             uniqueData[songs.artist.id] = songs
             // console.log('0424', songs)
 
@@ -106,15 +103,30 @@ search.addEventListener('input', () => {
             const searchTerm = search.value.toLowerCase() // Assumo che search sia l'input di ricerca
 
             const newArtistCol = document.createElement('div')
-            newArtistCol.classList.add('col', 'col-md-4')
+            newArtistCol.classList.add('col')
             newArtistCol.innerHTML = `
-                          <div class="card mb-4 shadow-sm">
-                            <img
-                              src="${songs.artist.picture}"
-                              class="bd-placeholder-img card-img-top"
-                            </img>
-                          </div>             
-                      `
+            <div class="card mb-4 shadow-sm d-flex">
+            <div class="d-flex justify-content-center">
+            <img
+              src="${songs.artist.picture_medium}"
+              class="bd-placeholder-img card-img-top rounded-circle m-2 shadow-lg"
+            />
+            </div>
+            <div class="card-body d-flex flex-column justify-content-between">
+              <p class="card-text text-nowrap text-truncate mb-0 mt-3 fw-bold fs-5">
+                ${songs.artist.name}
+              </p>
+              <p class="card-text text-nowrap text-truncate m-0 text-muted">
+                ${songs.artist.type}
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+               
+              </div>
+            </div>
+          </div>
+        `
 
             if (artistName.includes(searchTerm)) {
               // Se il nome dell'artista corrisponde alla ricerca, inseriscilo all'inizio
@@ -124,7 +136,7 @@ search.addEventListener('input', () => {
               artistRow.appendChild(newArtistCol)
             }
 
-            console.log('ciaoo', songs.artist.picture)
+            artistCardCount++
           }
         })
       })
@@ -136,6 +148,5 @@ search.addEventListener('input', () => {
     genres.classList.remove('d-none')
     row.classList.add('d-none')
     artistRow.classList.add('d-none')
-    mostRelevant.classList.add('d-none')
   }
 })

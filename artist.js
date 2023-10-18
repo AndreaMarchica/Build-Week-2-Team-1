c8687e3aac8dba2869e77480e691dec46cb1f9b0
AndreaMarchica <andrea.marchica94@gmail.com>
2023-10-18 22:33:22 +0200

    fix badges

--- artist.js
+++ artist.js
@@ -49,7 +49,20 @@ const generateArtist = (arrayOfSongs) => {
     // sezione alta info autore
     const artistInfo = document.getElementById("artist-info");
     artistInfo.innerHTML = `
-      <p class="mb-0"><i class="bi bi-patch-check-fill me-2"></i>Artista verificato</p>
+      <p class="mb-0"><svg width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect width='24' height='24' stroke='none' fill='#000000' opacity='0'/>
+
+
+      <g transform="matrix(0.42 0 0 0.42 12 12)" >
+      <g style="" >
+      <g transform="matrix(1 0 0 1 0 0)" >
+      <polygon style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(66,165,245); fill-rule: nonzero; opacity: 1;" points="5.62,-21 9.05,-15.69 15.37,-15.38 15.69,-9.06 21,-5.63 18.12,0 21,5.62 15.69,9.05 15.38,15.37 9.06,15.69 5.63,21 0,18.12 -5.62,21 -9.05,15.69 -15.37,15.38 -15.69,9.06 -21,5.63 -18.12,0 -21,-5.62 -15.69,-9.05 -15.38,-15.37 -9.06,-15.69 -5.63,-21 0,-18.12 " />
+      </g>
+      <g transform="matrix(1 0 0 1 -0.01 0.51)" >
+      <polygon style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1;" points="-2.6,6.74 -9.09,0.25 -6.97,-1.87 -2.56,2.53 7,-6.74 9.09,-4.59 " />
+      </g>
+      </g>
+      </g>
+      </svg>Artista verificato</p>
       <h1 style="font-size:3em; font-weight:bold">${data.artist.name}</h1>
       <a href="${data.artist.link}">Scopri su deezer</a>
       `;
@@ -106,11 +119,11 @@ const generateLikeTracks = function (arrayOfSongs) {
    <div>
      <button  id='icon-button'
        type="button"
-       class="position-relative border border-none rounded-circle"
+       class="position-relative border-none rounded-circle"
      >
        <i
          id="beppe"
-         class="bi bi-heart-fill text-white position-absolute translate-middle p-2 bg-success border border-light rounded-circle"
+         class="bi bi-heart-fill text-white position-absolute translate-middle p-2 bg-success border-light rounded-circle"
        >
        </i>
      </button>

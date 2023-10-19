1a52ad828f4ac6e63002a67de0f62772f95f3565
AndreaMarchica <andrea.marchica94@gmail.com>
2023-10-19 19:30:31 +0200

    responsiveness mobile

--- andrea_pier/script/artist.js
+++ andrea_pier/script/artist.js
@@ -1,57 +1,58 @@
 const addressBarContent = new URLSearchParams(location.search);
 
-const artistName = addressBarContent.get('artistName');
+const artistName = addressBarContent.get("artistName");
 
 const getData = function () {
-	const myUrl = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=' + artistName;
-	fetch(myUrl, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	})
-		.then((res) => {
-			if (res.ok) {
-				return res.json();
-			} else {
-				if (res.status === 404) {
-					throw new Error('404 - Not Found');
-				} else if (res.status === 500) {
-					throw new Error('500 - Internal Server Error');
-				} else {
-					throw new Error('Generic ERROR');
-				}
-			}
-		})
-		.then((song) => {
-			console.log('API SPOTY', song);
-			const data = song.data;
-			generateArtist(data);
-			generateLikeTracks(data);
-		})
-		.catch((err) => {
-			console.log('Si è verificato un errore:', err);
-		});
+  const myUrl =
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + artistName;
+  fetch(myUrl, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        if (res.status === 404) {
+          throw new Error("404 - Not Found");
+        } else if (res.status === 500) {
+          throw new Error("500 - Internal Server Error");
+        } else {
+          throw new Error("Generic ERROR");
+        }
+      }
+    })
+    .then((song) => {
+      console.log("API SPOTY", song);
+      const data = song.data;
+      generateArtist(data);
+      generateLikeTracks(data);
+    })
+    .catch((err) => {
+      console.log("Si è verificato un errore:", err);
+    });
 };
 getData();
 
 const generateArtist = (arrayOfSongs) => {
-	let numero = 1;
-	// variabile contatore track
-	arrayOfSongs.forEach((data) => {
-		const row = document.getElementById('special-row');
-		const newColTrack = document.createElement('div');
-		newColTrack.classList.add('col-8', 'my-2');
-		const newColRank = document.createElement('div');
-		newColRank.classList.add('col-2');
-		const newColDuration = document.createElement('div');
-		newColDuration.classList.add('col-2');
-		const backgroundArtistDiv = document.getElementById('background-artist');
-		backgroundArtistDiv.style.backgroundImage = `url(${data.artist.picture_xl})`;
-		backgroundArtistDiv.style.backgroundColor = 'gray';
+  let numero = 1;
+  // variabile contatore track
+  arrayOfSongs.forEach((data) => {
+    const row = document.getElementById("special-row");
+    const newColTrack = document.createElement("div");
+    newColTrack.classList.add("col-8", "my-2");
+    const newColRank = document.createElement("div");
+    newColRank.classList.add("col-2");
+    const newColDuration = document.createElement("div");
+    newColDuration.classList.add("col-2");
+    const backgroundArtistDiv = document.getElementById("background-artist");
+    backgroundArtistDiv.style.backgroundImage = `url(${data.artist.picture_xl})`;
+    backgroundArtistDiv.style.backgroundColor = "gray";
 
-		// sezione alta info autore
-		const artistInfo = document.getElementById('artist-info');
-		artistInfo.innerHTML = `
+    // sezione alta info autore
+    const artistInfo = document.getElementById("artist-info");
+    artistInfo.innerHTML = `
       <p class="mb-0"><svg width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect width='24' height='24' stroke='none' fill='#000000' opacity='0'/>
 
 
@@ -67,58 +68,58 @@ const generateArtist = (arrayOfSongs) => {
       </g>
       </svg>Artista verificato</p>
       <h1 style="font-size:3em; font-weight:bold">${data.artist.name}</h1>
-      <a href="${data.artist.link}">Scopri su deezer</a>
+      <a href="${data.artist.link}">Scopri su Deezer</a>
       `;
 
-		// creo la colonna track
-		newColTrack.innerHTML = `
+    // creo la colonna track
+    newColTrack.innerHTML = `
     <div class="d-flex align-items-center">
     <p class="m-0">${numero}</p>
     <img src="${data.album.cover_small}" class="mx-3">
     <p class="m-0">${data.title}</p>
     </div>
     `;
-		numero++;
-		//numero++ Per incrementare il numero artista
+    numero++;
+    //numero++ Per incrementare il numero artista
 
-		// creo la colonna rank
-		newColRank.innerHTML = `<div class="d-flex align-items-center h-100"><p class="m-0">${data.rank}</p></div>`;
+    // creo la colonna rank
+    newColRank.innerHTML = `<div class="d-flex align-items-center h-100"><p class="m-0">${data.rank}</p></div>`;
 
-		// creo la colonna duration
-		const time = `${data.duration}`;
-		const minutes = Math.floor(time / 60);
-		const seconds = (time % 60).toString().padStart(2, '0');
-		newColDuration.innerHTML = `<div class="d-flex align-items-center h-100"><p class="m-0">${minutes} : ${seconds}</p></div>`;
+    // creo la colonna duration
+    const time = `${data.duration}`;
+    const minutes = Math.floor(time / 60);
+    const seconds = (time % 60).toString().padStart(2, "0");
+    newColDuration.innerHTML = `<div class="d-flex align-items-center h-100"><p class="m-0">${minutes} : ${seconds}</p></div>`;
 
-		// appendo il tutto
-		row.appendChild(newColTrack);
-		row.appendChild(newColRank);
-		row.appendChild(newColDuration);
+    // appendo il tutto
+    row.appendChild(newColTrack);
+    row.appendChild(newColRank);
+    row.appendChild(newColDuration);
 
-		//  nascondo le track dalla 6 in poi
-		if (numero > 6) {
-			newColTrack.classList.add('d-none');
-			newColRank.classList.add('d-none');
-			newColDuration.classList.add('d-none');
-		}
+    //  nascondo le track dalla 6 in poi
+    if (numero > 6) {
+      newColTrack.classList.add("d-none");
+      newColRank.classList.add("d-none");
+      newColDuration.classList.add("d-none");
+    }
 
-		// pulsante mostra altro
-		const showButton = document.getElementById('show');
-		showButton.addEventListener('click', function () {
-			newColTrack.classList.remove('d-none');
-			newColRank.classList.remove('d-none');
-			newColDuration.classList.remove('d-none');
-			showButton.classList.add('d-none');
-		});
-	});
+    // pulsante mostra altro
+    const showButton = document.getElementById("show");
+    showButton.addEventListener("click", function () {
+      newColTrack.classList.remove("d-none");
+      newColRank.classList.remove("d-none");
+      newColDuration.classList.remove("d-none");
+      showButton.classList.add("d-none");
+    });
+  });
 };
 
 // creo la colonna dei like
 const generateLikeTracks = function (arrayOfSongs) {
-	arrayOfSongs.forEach((data) => {
-		const imageCol = document.getElementById('like-image');
-		const textCol = document.getElementById('like-text');
-		imageCol.innerHTML = `
+  arrayOfSongs.forEach((data) => {
+    const imageCol = document.getElementById("like-image");
+    const textCol = document.getElementById("like-text");
+    imageCol.innerHTML = `
    <div class='row d-flex justify-content-between'>
 	 <div class=" rounded-pill position-relative col-3">
 	 <img
@@ -140,15 +141,19 @@ const generateLikeTracks = function (arrayOfSongs) {
 </div>
    </div>
  `;
-		const iconImage = document.getElementById('icon-button');
+    const iconImage = document.getElementById("icon-button");
 
-		textCol.innerHTML = ``;
-	});
+    textCol.innerHTML = ``;
+  });
 };
 
-// const showButton = document.getElementById("show");
-// const showAll = function () {
-//   newColTrack.classList.remove("d-none");
-//   newColRank.classList.remove("d-none");
-//   newColDuration.classList.remove("d-none");
-// };
+// bottone play inizio pagina
+const playBtn = () => {
+  const playBtn = document.querySelector("#play");
+
+  playBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("play");
+  });
+};

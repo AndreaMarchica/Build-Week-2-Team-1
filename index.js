46b8d6c0f2ab33ed773e94f317827b942c44fea4
OleksandrLarionov <larionov.alex23@gmail.com>
2023-10-20 09:48:58 +0200

    bottons n cards

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+let songsCardCount = 0;
+let songsCardCount2 = 0;
+
 const generatePlaylistsCard = function (arrayOfSongs) {
 	const rowPlaylist = document.getElementById('carousel');
 	arrayOfSongs.forEach((data) => {
 		const fullName = data.artist.name.replace(/ /g, '');
 		const newCol = document.createElement('div');
 		newCol.classList.add('carousel-item');
+
 		newCol.innerHTML = `	
    
     <!-- qui va la seconda card -->
@@ -88,6 +92,65 @@ const close = () => {
 	});
 };
 close();
+
+const buondi = (song) => {
+	const buon = document.getElementById('buongiorno');
+	song.forEach((element) => {
+		const newCol = document.createElement('div');
+		newCol.classList.add('col', 'col-md-4');
+		if (songsCardCount < 6) {
+			newCol.innerHTML = `
+  
+  <div class="card mb-3" style="max-width: 540px">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img
+          src="${element.album.cover_big}"
+          class="img-fluid rounded-start"
+          alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h6 class="card-title">${element.album.title}</h6>
+        </div>
+      </div>
+    </div>
+ 
+</div>
+  `;
+
+			buon.appendChild(newCol);
+		}
+		songsCardCount++;
+	});
+};
+
+const others = (song) => {
+	const others = document.getElementById('others');
+	song.forEach((element) => {
+		const newCol = document.createElement('div');
+		newCol.classList.add('col', 'col-md-2');
+		if (songsCardCount2 < 5) {
+			newCol.innerHTML = `
+  
+  		<div class="card" >
+									<img
+										src="${element.artist.picture}"
+										class="card-img-top"
+										alt="..." />
+									<div class="card-body">
+										<h6 class="card-title">${element.artist.name}</h6>
+										<p>di Spotify</p>
+									</div>
+								</div>
+  `;
+
+			others.appendChild(newCol);
+		}
+		songsCardCount2++;
+	});
+};
+
 const getData = function () {
 	const myUrl = 'https://striveschool-api.herokuapp.com/api/deezer/search?q=celentano';
 	fetch(myUrl, {
@@ -112,8 +175,10 @@ const getData = function () {
 			console.log('API SPOTY', song);
 			const data = song.data;
 			generatePlaylistsCard(song.data);
-			generateGroupCard(song.data);
-			modifyAudioBar(song.data);
+			others(data);
+			// generateGroupCard(song.data);
+			// modifyAudioBar(song.data);
+			buondi(data);
 		})
 		.catch((err) => {
 			console.log('Si è verificato un errore:', err);
@@ -121,53 +186,53 @@ const getData = function () {
 };
 getData();
 
-const generateGroupCard = function (arrayOfSongs) {
-	const rowGroup = document.getElementById('group-row');
-	arrayOfSongs.forEach((data) => {
-		const newCol = document.createElement('div');
-		// newCol.classList.add("col", "col-6", "col-sm-4", "col-md-3", "col-lg-2");
-		newCol.innerHTML = `<div class="row">
-        <div class="col-sm-4 mb-3 mt-3 mb-sm-0">
-          <div class="card d-flex flex-row">
-          <img src="${data.artist.picture_medium}" class="card-img-left" alt="...">
-            <div class="card-body d-flex text-center justify-content-center align-items-center">
-              <h5 class="card-title ">Special title treatment</h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4 mt-3">
-        <div class="card d-flex flex-row">
-        <img src="${data.album.cover_medium}" class="card-img-left" alt="...">
-            <div class="card-body d-flex text-center justify-content-center align-items-center">
-              <h5 class="card-title">Special title treatment</h5>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-4 mt-3">
-        <div class="card d-flex flex-row">
-        <img src="${data.album.cover_medium}" class="card-img-left" alt="...">
-            <div class="card-body d-flex text-center justify-content-center align-items-center">
-              <h5 class="card-title">Special title treatment</h5>
-            </div>
-          </div>
-        </div>
-      </div>`;
-		rowGroup.appendChild(newCol);
-	});
-};
+// const generateGroupCard = function (arrayOfSongs) {
+// 	const rowGroup = document.getElementById('group-row');
+// 	arrayOfSongs.forEach((data) => {
+// 		const newCol = document.createElement('div');
+// 		// newCol.classList.add("col", "col-6", "col-sm-4", "col-md-3", "col-lg-2");
+// 		newCol.innerHTML = `<div class="row">
+//         <div class="col-sm-4 mb-3 mt-3 mb-sm-0">
+//           <div class="card d-flex flex-row">
+//           <img src="${data.artist.picture_medium}" class="card-img-left" alt="...">
+//             <div class="card-body d-flex text-center justify-content-center align-items-center">
+//               <h5 class="card-title ">Special title treatment</h5>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="col-sm-4 mt-3">
+//         <div class="card d-flex flex-row">
+//         <img src="${data.album.cover_medium}" class="card-img-left" alt="...">
+//             <div class="card-body d-flex text-center justify-content-center align-items-center">
+//               <h5 class="card-title">Special title treatment</h5>
+//             </div>
+//           </div>
+//         </div>
+//         <div class="col-sm-4 mt-3">
+//         <div class="card d-flex flex-row">
+//         <img src="${data.album.cover_medium}" class="card-img-left" alt="...">
+//             <div class="card-body d-flex text-center justify-content-center align-items-center">
+//               <h5 class="card-title">Special title treatment</h5>
+//             </div>
+//           </div>
+//         </div>
+//       </div>`;
+// 		rowGroup.appendChild(newCol);
+// 	});
+// };
 
 // AUDIO BAR
-const modifyAudioBar = function (arrayOfSongs) {
-	arrayOfSongs.forEach((data) => {
-		const trackTitle = document.querySelector('h3');
-		trackTitle.innerHTML = `<h3>${data.title}</h3>`;
-		const artist = document.querySelector('div.audio-details > p');
-		artist.innerHTML = `<p>${data.artist.name}</p>`;
-		const audioImage = document.querySelector('div.audio-player > img');
-		audioImage.src = `${data.album.cover_medium}`;
-		const track = document.querySelector('div.react-player > audio');
-		track.src = `${data.preview}`;
-		// const deleteMark = document.querySelector("div.commonninja-ribbon");
-		// deleteMark.classList.add("d-none !important");
-	});
-};
+// const modifyAudioBar = function (arrayOfSongs) {
+// 	arrayOfSongs.forEach((data) => {
+// 		const trackTitle = document.querySelector('h3');
+// 		trackTitle.innerHTML = `<h3>${data.title}</h3>`;
+// 		const artist = document.querySelector('div.audio-details > p');
+// 		artist.innerHTML = `<p>${data.artist.name}</p>`;
+// 		const audioImage = document.querySelector('div.audio-player > img');
+// 		audioImage.src = `${data.album.cover_medium}`;
+// 		const track = document.querySelector('div.react-player > audio');
+// 		track.src = `${data.preview}`;
+// 		// const deleteMark = document.querySelector("div.commonninja-ribbon");
+// 		// deleteMark.classList.add("d-none !important");
+// 	});
+// };
